8675cc67e0c4c3210ace566d2f2072919e1595f0
Lucid Dreamer <48848408+LucidDream-J@users.noreply.github.com>
2021-07-10 00:04:59 +0800

    Add files via upload

---
+++ TEAM/resources/js/scripts.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+  /** for sticky navigation */
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "60px;" }
+  );
+
+  /** btn Scroll to required section */
+  $(".js--scroll-to-plan").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
+
+  /**Navigation-Scrool  works with ids and id hrefs*/
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
+
+  /** Animation on Scroll  js--wp-1*/
+
+  /* 
+var waypoint = new Waypoint({
+  element: document.getElementById('px-offset-waypoint'),
+  handler: function(direction) {
+    notify('I am 20px from the top of the window')
+  },
+  offset: 20 
+})
+*/
+
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  /* Mobile navigation */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon i");
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
+});
