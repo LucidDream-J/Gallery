85a1f6a39b17493247e5313a0b9d3035a56bf421
Lucid Dreamer <48848408+LucidDream-J@users.noreply.github.com>
2023-09-19 21:23:06 +0800

    Add files via upload

---
+++ longScroll/script3.js
@@ -0,0 +1,103 @@
+const main = document.querySelector('main')
+const textarea = document.getElementById('text')
+
+const data3 = [
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-100-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-91-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-92-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-93-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-94-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-95-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-96-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-97-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-98-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-99-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-72-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-73-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-77-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-82-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-16-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-28-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-29-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-75-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-70B-min.jpg',
+      text: 'They took out we put back in!',
+    },
+    {
+      image: '../images/RePopulate/longScrolIndigi/Australian_indigenous_people_in_landscape-71-min.jpg',
+      text: 'They took out we put back in!',
+    },
+  ]
+  
+  data3.forEach(createBox)
+  
+  // Create speech boxes
+  function createBox(item) {
+    const box = document.createElement('div')
+  
+    const { image, text } = item
+  
+    box.classList.add('box')
+  
+    box.innerHTML = `
+      <img src="${image}" alt="${text}" />
+      <p class="info">${text}</p>
+    `
+    main.appendChild(box)
+  }
+  
